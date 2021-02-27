aad7dd5ec5fc74652590465faa65d500003b9b0f
brunocamposalves <brunocamposalves@hotmail.com>
2021-02-27 16:57:20 -0300

    refactor: model Iventory

--- inventories/urls.py
+++ inventories/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import InventoryView, RefuelView
 
 urlpatterns = [
-    path("inventories/<str:category>/", InventoryView.as_view()),
-    path("inventories/refuel/<int:inventory_id>/", RefuelView.as_view())
+    path("inventories/", InventoryView.as_view()),
+    path("inventories/refuel/<int:product_id>/", RefuelView.as_view())
 ]
--- inventories/views.py
+++ inventories/views.py
@@ -9,17 +9,7 @@ from .serializers import InventorySerializer
 
 
 class InventoryView(APIView):
-    def get(self, request, category=""):
-        if category:
-            try:
-                inventories_list = Inventory.objects.get(category=category)
-            except ObjectDoesNotExist:
-                return Response({"message": "does not have products in the inventory of this category"}, status=status.HTTP_404_NOT_FOUND)
-
-            serializer = InventorySerializer(inventories_list, many=True)
-
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
+    def get(self, request):
         inventories_list = Inventory.objects.all()
         serializer = InventorySerializer(inventories_list, many=True)
 
@@ -27,11 +17,11 @@ class InventoryView(APIView):
 
 
 class RefuelView(APIView):
-    def put(self, request, inventory_id=""):
+    def put(self, request, product_id=""):
         amount = request.data.get("amount")
 
         try:
-            inventory = Inventory.objects.get(id=inventory_id)
+            inventory = Inventory.objects.get(id=product_id)
         except ObjectDoesNotExist:
             return Response({"message": "does not have products in the inventory"}, status=status.HTTP_404_NOT_FOUND)
 
