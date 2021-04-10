3d008e754dca06e3a00e5549a8fe5110e9468f8c
eduardo <eduardo.magno.u@gmail.com>
2021-04-10 13:47:41 -0300

    redis implemented in inventories route

--- products/views.py
+++ products/views.py
@@ -1,23 +1,17 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import authentication, permissions
-from products.services.register import register_product
 from accounts.permissions import IsSalesman
-# Create your views here.
 
 from rest_framework.viewsets import GenericViewSet
-from shared.pagination import CustomLimitOffsetPagination
 from .mixins import RegisterMixin
 from .models import Product
 from .serializers import ProductSerializer
 from shared.pagination import CustomLimitOffsetPagination
 
+
 class ProductView(GenericViewSet, RegisterMixin):
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAdminUser | IsSalesman]
-    queryset= Product.objects.all().order_by('id')
+    queryset = Product.objects.all().order_by('id')
     serializer_class = ProductSerializer
     pagination_class = CustomLimitOffsetPagination
     ordering = ['id']
-
- 
